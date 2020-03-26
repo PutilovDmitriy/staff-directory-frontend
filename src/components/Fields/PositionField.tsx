fa7a334aa72fe7f7b889f00549514a39b0a72821
Putilov Dmitriy <dmitii.98.putilov@gmail.com>
2020-03-26 21:07:22 +0500

    -

--- src/components/Fields/PositionField.tsx
+++ src/components/Fields/PositionField.tsx
@@ -1,21 +1,8 @@
 import React, { ChangeEvent } from "react";
 import MenuItem from "@material-ui/core/MenuItem";
 import TextField from "@material-ui/core/TextField";
+import { positions } from "../../consts";
 
-const positions: { id: number; name: string }[] = [
-  { id: 1, name: "Директор" },
-  { id: 2, name: "Менеджер" },
-  { id: 3, name: "Инженер" },
-  { id: 4, name: "Стажёр" },
-  { id: 5, name: "Охраник" },
-  { id: 6, name: "Уборщик" },
-  { id: 7, name: "Администратор" },
-  { id: 8, name: "Водитель" }
-];
-
-interface IPosition {
-  position: string | null;
-}
 interface Props {
   position: string | null;
   handleChange: (event: ChangeEvent<HTMLInputElement>) => void;
--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,8 +1,16 @@
-import { Worker } from './../redux/types/Worker';
+import { Worker } from "./../redux/types/Worker";
 
-export const url: string = 'https://afternoon-wave-94253.herokuapp.com/staff/';
+export const url: string = "https://afternoon-wave-94253.herokuapp.com/staff/";
 
-export const bildWorkerObj = (id: number, FIO: string, position: string, date: string, gender: string, isFired: boolean, colleagues: string[]) => {
+export const bildWorkerObj = (
+  id: number,
+  FIO: string,
+  position: string,
+  date: string,
+  gender: string,
+  isFired: boolean,
+  colleagues: string[]
+) => {
   return {
     id: id,
     FIO: FIO,
@@ -10,25 +18,39 @@ export const bildWorkerObj = (id: number, FIO: string, position: string, date: s
     birthday: date,
     gender: gender,
     isFired: isFired,
-    colleagues: colleagues,
-  }
-}
+    colleagues: colleagues
+  };
+};
 
-export const gettingActiveWorkerData = (id: number | null | undefined, staff: Worker[] | undefined): Worker | undefined => {
-  if (id !== null !== undefined && staff !== undefined) {
-    const data = staff.find(item => item.id === id)
+export const gettingActiveWorkerData = (
+  id: number | null | undefined,
+  staff: Worker[] | undefined
+): Worker | undefined => {
+  if ((id !== null) !== undefined && staff !== undefined) {
+    const data = staff.find(item => item.id === id);
     if (data !== undefined) {
       return data;
     } else {
       return {
         id: 0,
-        FIO: '',
-        position: '',
-        birthday: '',
+        FIO: "",
+        position: "",
+        birthday: "",
         gender: "famela",
         isFired: false,
         colleagues: []
-      }
+      };
     }
-  } else return undefined
-}
+  } else return undefined;
+};
+
+export const positions: { id: number; name: string }[] = [
+  { id: 1, name: "Директор" },
+  { id: 2, name: "Менеджер" },
+  { id: 3, name: "Инженер" },
+  { id: 4, name: "Стажёр" },
+  { id: 5, name: "Охраник" },
+  { id: 6, name: "Уборщик" },
+  { id: 7, name: "Администратор" },
+  { id: 8, name: "Водитель" }
+];
